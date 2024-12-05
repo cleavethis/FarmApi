5e55f4c82df092640800625e7a5dde0e3015f003
Rick <111731365+cleavethis@users.noreply.github.com>
2024-12-05 14:15:45 -0800

    Add project files.

---
+++ FarmApi/Controllers/FarmsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FarmApi.Models;
+
+namespace FarmApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FarmsController : ControllerBase
+    {
+        private readonly FarmContext _context;
+
+        public FarmsController(FarmContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("FindGrower")]
+        public IActionResult FindGrower(string searchTarget) { 
+
+            var growers = _context.Growers.Where(grower=> grower.GrowerName.Contains(searchTarget)).ToList();
+
+            if (growers == null)
+                return NotFound();
+
+            return Ok(growers);
+        
+        }
+
+        [HttpGet("GetFields")] 
+        
+        public IActionResult GetFields(Guid growerID)
+        {
+            var grower = _context.Growers.Include(g => g.Fields).FirstOrDefault(g => g.GrowerID == growerID);
+
+            if (grower == null)
+                return NotFound();
+
+            return Ok(grower.Fields);
+        }
+      
+    }
+}
---
+++ FarmApi/Models/FarmContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FarmApi.Models;
+
+namespace FarmApi.Models
+{
+    public class FarmContext : DbContext
+    {
+        public FarmContext(DbContextOptions<FarmContext> options) : base(options) { }
+        public DbSet<Grower> Growers { get; set; }
+        public DbSet<Field> Fields { get; set; }
+
+        public class Grower
+        {
+            public Guid GrowerID { get; set; }
+            public string GrowerName { get; set; }
+            public List<Field> Fields { get; set; }
+        }
+
+        public class Field
+        {
+            public Guid FieldID { get; set; }
+            public string FieldName { get; set; }
+            public Guid GrowerID { get; set; } 
+        }
+      
+    }
+}
